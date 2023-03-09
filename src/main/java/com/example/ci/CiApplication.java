4c23a7bcaa0bb72d4fe69fc40086226bc14c2aaa
Jinwook Kim <kimjinwook94@gmail.com>
2023-03-09 21:54:04 +0900

    test

--- src/main/java/com/example/ci/CiApplication.java
+++ src/main/java/com/example/ci/CiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CiApplication {
-
+// Test
 	public static void main(String[] args) {
 		SpringApplication.run(CiApplication.class, args);
 	}
