ac88171cb5b3c9860bbafa2e82b4f98d734a70f5
Jinwook <44575214+Jinwook94@users.noreply.github.com>
2023-03-10 23:52:26 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 REPOSITORY=/home/ec2-user/deployment
 cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep "SNAPSHOT.jar" | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH &
+nohup java -jar $JAR_PATH > /home/ec2-user/deployment/logfile.log 2>&1 &
